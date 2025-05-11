f679d536233adaddae1e8285c0d6fa8b95de6918
Akinola Femi <akinolafemi573@gmail.com>
2025-05-11 20:32:28 +0100

    Categories error resolved

--- src/app/pages/categories/page.jsx
+++ src/app/pages/categories/page.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Categories from '@/app/Components/Category/Categories'
 import { BannerSlider } from '@/app/Components/Category/BannerSlider'
 
