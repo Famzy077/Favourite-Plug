8d18686f20f96f66c57e8e53bdea6855bb640293
Akinola Femi <akinolafemi573@gmail.com>
2025-06-10 23:29:38 +0100

    update the import path

--- src/app/(pages)/admin/dashboard/page.jsx
+++ src/app/(pages)/admin/dashboard/page.jsx
@@ -2,8 +2,8 @@
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ProductManagement } from "@/app/Components/admin/ProductManagement";
-import { UserManagement } from "@/app/components/admin/UserManagement";
-import { DashboardOverview } from "@/app/components/admin/DashboardOverview";
+import { UserManagement } from "../../..//components/admin/UserManagement";
+import { DashboardOverview } from "../../../Components/admin/DashboardOverview";
 
 export default function AdminDashboardPage() {
   return (
