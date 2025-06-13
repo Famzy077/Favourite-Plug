7b380afc5c48ebee2629cf8fa55c2d2d84f7abdc
Akinola Femi <akinolafemi573@gmail.com>
2025-06-13 09:58:33 +0100

    fix bug issue for production

--- src/app/(auth)/reset-password/page.jsx/page.jsx
+++ src/app/(auth)/reset-password/page.jsx/page.jsx
@@ -1,5 +1,7 @@
+'use client';
 import { useState, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
+import axios from 'axios';
 
 export const ResetPasswordPage = () => {
 
