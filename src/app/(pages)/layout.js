373a032d7c515b3504083cab7cc87a0565d1eaa8
Akinola Femi <akinolafemi573@gmail.com>
2025-06-30 02:43:10 +0100

    Add chat agent

--- src/app/(pages)/layout.js
+++ src/app/(pages)/layout.js
@@ -56,6 +56,20 @@ export default function PageLayout({ children }) {
             },
           }}
         />
+
+        <Script id="tawk-to-script" strategy="lazyOnload">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6862754f47ce5c1911cb02ca/1iv09kfp7';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
